f5ffdfeb3fe58adde68eadb7f8eb632fd06ae14c
14 <3262128162@qq.com>
2020-05-16 13:45:32 +0800

    测试一下阿里

---
+++ sss.sql
@@ -0,0 +1,101 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.29-log : Database - warehouse
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`warehouse` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `warehouse`;
+
+/*Table structure for table `menu` */
+
+DROP TABLE IF EXISTS `menu`;
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `order_num` varchar(255) DEFAULT NULL,
+  `parent_id` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `menu` */
+
+insert  into `menu`(`id`,`name`,`order_num`,`parent_id`,`type`,`url`) values (1,'仓库管理','1','0','1','/two'),(2,'用户管理','2','0','1','/user.do'),(3,'权限管理','3','0','1','/menu.do');
+
+/*Table structure for table `menu_role` */
+
+DROP TABLE IF EXISTS `menu_role`;
+
+CREATE TABLE `menu_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `menu_id` varchar(255) DEFAULT NULL,
+  `role_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `menu_role` */
+
+insert  into `menu_role`(`id`,`menu_id`,`role_id`) values (1,'1','1'),(2,'2','1'),(3,'3','2'),(4,'3','1');
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `role` */
+
+insert  into `role`(`id`,`role_name`) values (1,'系统管理员'),(2,'普通用户');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `worknum` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`nickname`,`username`,`worknum`,`password`) values (1,'admin','admin','1000','$2a$10$61m4WYkvNWcwnRL0gOLRg.uA7Xl7ydhhXINnbxYKNFEGFOABqzL1a'),(2,'fdaf','sang','1001','$2a$10$61m4WYkvNWcwnRL0gOLRg.uA7Xl7ydhhXINnbxYKNFEGFOABqzL1a'),(3,NULL,'pzg','1234','$2a$10$H0z5Tt.9SGESI1dbAN6t2.c8eKtFiZ5QhrAFDgRSHwkQOKpZgmaVa'),(4,NULL,'pzg1','1233','$2a$10$4qyZyiM3NH57lXMe.v4deucDdYIEOPIU2IDRQL9GF68N0dBQZCiae'),(5,NULL,'admin3','1111','$2a$10$HcaHA74HRG.Cp.3ctm9vnOHy5Khsr.hBdcFa7SsGHgdCQqbhJdnQ6'),(6,NULL,'admin5','1367','$2a$10$0Q2oZ62dHfWTYlSteclyCOQz8/INxq5oPDRm2E3KD4v4GO7h1alJy');
+
+/*Table structure for table `user_role` */
+
+DROP TABLE IF EXISTS `user_role`;
+
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `role_id` varchar(255) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user_role` */
+
+insert  into `user_role`(`id`,`role_id`,`user_id`) values (1,'1','1'),(2,'2','2'),(3,'2','6');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
